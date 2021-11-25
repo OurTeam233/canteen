e7f1ede170f56c75f8a4fa693e753b9caf21f97f
crsecent <1208047186@qq.com>
2021-11-25 19:11:16 +0800

    试一下在vscode里面直接提交(登录输入框)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <!-- <el-form ref="form" :model="form" label-width="80px">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="账号">
+        <el-input type="text" placeholder="请输入用户名" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="onSubmit">登录</el-button>
 
-
-    </el-form> -->
-    哈哈哈，我是登录界面
-
+    </el-form>
+    
 
 
   </div>
@@ -13,7 +18,14 @@
 <script>
   export default {
     name: 'Login',
-    
+    data(){
+      return {
+        form: {
+          name: '',
+          password: ''
+        }
+      }
+    },
     methods: {
       onSubmit() {
         console.log("submit!");
