d1823bf63a0398864c42ef1b75b3bf181e44f16e
crsecent <1208047186@qq.com>
2021-12-09 19:49:00 +0800

    完成登录拦截模块

--- src/main.js
+++ src/main.js
@@ -3,57 +3,21 @@ import App from './App.vue'
 /* 使用elementUI组件 */
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// 让Vue使用这个路由
-import VueRouter from 'vue-router';
 // Vue实例中使用的路由
 import router from './router/index.js';
-
 import store from './store/index.js';
 // 实验一下echarts(全局导入)
 import echarts from "echarts";
 Vue.prototype.$echarts = echarts;
-
+/* 让vue使用elementUI(懒得抽了) */
+Vue.use(ElementUI);
 
  
 
-/* 让vue使用elementUI */
-Vue.use(ElementUI);
-/* 让vue使用router */
-Vue.use(VueRouter);
 
 
 
 Vue.config.productionTip = false
-
-
-// 路由守卫
-// router.beforeEach((to, from, next) => {
-//   let isLogin = sessionStorage.getItem('isLogin');
-//   // 退出登录
-//   if(to.path == '/logout'){
-//       // 将缓存清空
-//       sessionStorage.clear();
-//       next({path:'/login'})
-//   } else if(to.path == '/login'){
-//       // 如果已经登录，就跳转到首页
-//       if(isLogin){
-//           next({path:'/main'})
-//       } else {
-//           next()
-//       }
-//   } else if(isLogin == null){
-//       // 如果没有登录，就跳转到登录页面
-//       next({path:'/login'})
-//   }
-      
-// })
-
-
-
-
-
-
-
 
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
+
+// 引入加载条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 配置
+NProgress.inc()
+NProgress.configure({
+    easing: 'ease', // 动画方式
+    speed: 800, // 递增进度条的速度
+    showSpinner: false, // 是否显示加载ico
+    trickleSpeed: 200, // 自动递增间隔
+    minimum: 0.1 // 初始化时的最小百分比
+})
 
 
 Vue.use(VueRouter)
-var title = '| 智慧餐口';
+var title = ' | 智慧餐口';
 //将路由单独抽出来
-const routes = [
-    {
+const routes = [{
         //默认首页
         path: '',
         //redirect重定向
-        redirect:'login',
+        redirect: 'login',
         component: () => import('../views/Login.vue'),
     },
     {
@@ -18,47 +32,53 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: () => import('../views/Login.vue'),
+        meta: {
+            title: '登录' + title
+        }
     },
     {
         // 404页面
-        path:'*',
-        name:'NotFound',
+        path: '*',
+        name: 'NotFound',
         component: () => import('../views/NotFound.vue'),
+        meta: {
+            title: '页面未找到呢'
+        }
     },
-    
+
     {
         //订餐页面
         path: '/ordering',
         name: 'Ordering',
         component: () => import('../Layout/Ordering.vue'),
-        children:[
+        children: [
             // 订餐
             {
-                path:'newOrders',
-                name:'NewOrders',
+                path: 'newOrders',
+                name: 'NewOrders',
                 component: () => import('../components/AllOrder/NewOrders.vue'),
                 meta: {
                     title: '新增订单' + title
                 }
             },
             {
-                path:'getOrders',
-                name:'GetOrders',
+                path: 'getOrders',
+                name: 'GetOrders',
                 component: () => import('../components/AllOrder/GetOrders.vue'),
             },
             {
-                path:'completeOrders',
-                name:'CompleteOrders',
+                path: 'completeOrders',
+                name: 'CompleteOrders',
                 component: () => import('../components/AllOrder/CompleteOrders.vue'),
             },
             {
-                path:'illegalOrders' ,
-                name:'IllegalOrders',
+                path: 'illegalOrders',
+                name: 'IllegalOrders',
                 component: () => import('../components/AllOrder/IllegalOrders.vue'),
             },
             {
-                path:'menu',
-                name:'Menu',
+                path: 'menu',
+                name: 'Menu',
                 component: () => import('../components/AllMenu/Menu.vue'),
                 meta: {
                     title: '菜单管理' + title
@@ -70,27 +90,27 @@ const routes = [
                 component: () => import('../components/AllMenu/Meat.vue'),
             },
             {
-                path:'Test/:id',
+                path: 'Test/:id',
                 name: 'Test',
                 component: () => import('../components/Statistics/Test.vue'),
-                props:true,
+                props: true,
             },
             {
-                path:'OverallStatistics',
+                path: 'OverallStatistics',
                 name: 'OverallStatistics',
                 component: () => import('../components/Statistics/OverallStatistics.vue'),
                 meta: {
                     title: '统计总览' + title
                 }
-                
+
             },
             {
-                path:'editMenu',
+                path: 'editMenu',
                 name: 'EditMenu',
                 component: () => import('../components/AllMenu/EditMenu.vue'),
             },
             {
-                path:'info',
+                path: 'info',
                 name: 'Info',
                 component: () => import('../components/Information/Info.vue'),
                 meta: {
@@ -105,40 +125,62 @@ const routes = [
 const originalPush = VueRouter.prototype.push
 //修改原型对象中的push方法
 VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err)
+    return originalPush.call(this, location).catch(err => err)
 }
 
 const router = new VueRouter({
-  routes,
-  mode: 'history',
+    routes,
+    mode: 'history',
 })
 
 
 
 
-// 路由守卫
-// router.beforeEach((to, from, next) => {
-//     let isLogin = sessionStorage.getItem('isLogin');
-//     // 退出登录
-//     if(to.path == '/logout'){
-//         // 将缓存清空
-//         sessionStorage.clear();
-//         next({path:'/login'})
-//     } else if(to.path == '/login'){
-//         // 如果已经登录，就跳转到首页
-//         if(isLogin){
-//             next({path:'/main'})
-//         } else {
-//             next()
-//         }
-//     } else if(isLogin == null){
-//         // 如果没有登录，就跳转到登录页面
-//         next({path:'/login'})
-//     }
-        
-// })
+// 路由守卫(登录登出跳转)
+router.beforeEach((to, from, next) => {
+
+    //设置标题
+    let title = to.meta.title;
+    if (title) {
+        document.title = title
+    }
+
+
+    let isLogin = sessionStorage.getItem('isLogin');
+    // 退出登录
+    if (to.path == '/logout') {
+        // 将缓存清空
+        sessionStorage.clear();
+        next({
+            path: '/login'
+        })
+    } else if (to.path == '/login') {
+        // 如果已经登录，就跳转到首页
+        if (isLogin) {
+            next({
+                path: '/ordering'
+            })
+        } else {
+            next()
+        }
+    } else if (isLogin == null) {
+        // 如果没有登录，就跳转到登录页面
+        next({
+            path: '/login'
+        })
+    } else {
+        next()
+    }
+
+})
+
+
+// 挂载路由守卫 - 访问结束
+router.afterEach(() => {
+    // 关闭加载条
+    NProgress.done()
+})
 
 
 
 export default router
-
