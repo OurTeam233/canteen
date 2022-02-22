2c5b3c68e898d6b8c6f140730e5ae4d7a289cc39
crsecent <1208047186@qq.com>
2022-02-22 20:44:38 +0800

    最新版

--- src/api/user.js
+++ src/api/user.js
@@ -7,26 +7,26 @@ import axios from '../utils/request'
 // 用户登录
 // export const login = (username, password, userType) => http.post('http://121.43.56.241:8080/CanteenWeb/User/Login', {username, password, userType})
 // export const login = (username, password, userType) => api.postListAPI(username, password, userType)
-export const login = (username, password, userType) => axios.post('/CanteenWeb/Login', {username, password, userType})
+export const login = (username, password, userType) => axios.post('/CanteenWeb/LoginAction/', {username, password, userType})
 
 
 // 获取店铺数据
-export const getStoreInfo = () => axios.post('/CanteenWeb/Store')
+export const getStoreInfo = () => axios.post('/CanteenWeb/StoreAction/')
 
 // 获取店铺中所有的菜品信息
-export const getDishesList = () => axios.post('/CanteenWeb/Dishes')
+export const getDishesList = () => axios.post('/CanteenWeb/DishesAction/')
 
 // 获取...订单信息
-export const getOrderList = () => axios.post('/CanteenWeb/Order/Select')
+export const getOrderList = () => axios.post('/CanteenWeb/OrderAction/Select')
 
 // 更改订单状态
-export const changeOrderType = (orderId, type) => axios.post('/CanteenWeb/Order/Update', {orderId, type})
+export const changeOrderType = (orderId, type) => axios.post('/CanteenWeb/OrderAction/Update', {orderId, type})
 
 // 删除菜品
-export const deleteDishes = (dishesId) => axios.post('/CanteenWeb/Dishes/Delete', {dishesId})
+export const deleteDishes = (dishesId) => axios.post('/CanteenWeb/DishesAction/Delete', {dishesId})
 
 // 新增菜品
-export const addDishes = (dishes) => axios.post('/CanteenWeb/Dishes/Insert',{dishes})
+export const addDishes = (dishes) => axios.post('/CanteenWeb/DishesAction/Insert',{dishes})
 // export const addDishes = (dishes) => axios({
 //   url: '/CanteenWeb/Dishes/Insert',
 //   method: 'post',
@@ -36,50 +36,50 @@ export const addDishes = (dishes) => axios.post('/CanteenWeb/Dishes/Insert',{dis
 // })
 
 // 查询所有菜品标签
-export const getDishesType = () => axios.post('/CanteenWeb/Dishes/Types/Select')
+export const getDishesType = () => axios.post('/CanteenWeb/DishesAction/Types/Select')
 
 // 上传图片(好像被组件代替了没用上)
-export const uploadImg = (file) => axios.post('/upload', {file})
+export const uploadImg = (file) => axios.post('/uploadAction/', {file})
 
 // 修改店铺营业状态
-export const changeStoreStatus = (status) => axios.post('/CanteenWeb/Store/Update', {status})
+export const changeStoreStatus = (status) => axios.post('/CanteenWeb/StoreAction/Update', {status})
 
 
 
 /*管理员操作 */
 
 // 查询所有用户
-export const getUserList = () => axios.post('/CanteenWeb/User/Select')
+export const getUserList = () => axios.post('/CanteenWeb/UserAction/Select')
 
 // 修改用户账号和密码
-export const resetUser = (user) => axios.post('/CanteenWeb/User/Update', {user}) 
+export const resetUser = (user) => axios.post('/CanteenWeb/UserAction/Update', {user}) 
 
 // 创建新的商家用户
-export const createUser = (user) => axios.post('/CanteenWeb/User/Insert', {user})
+export const createUser = (user) => axios.post('/CanteenWeb/UserAction/Insert', {user})
 
 
 /*统计界面操作 (商家和管理员公用界面，只是请求到的数据不同) */
 
 //获取明天的预定菜品
-export const getTomorrowDishes = () => axios.post('/CanteenWeb/Statistic/Dishes/NextDay')
+export const getTomorrowDishes = () => axios.post('/CanteenWeb/StatisticAction/Dishes/NextDay')
 
 //获取今天的预定菜品
-export const getTodayDishes = () => axios.post('/CanteenWeb/Statistic/Dishes/Today')
+export const getTodayDishes = () => axios.post('/CanteenWeb/StatisticAction/Dishes/Today')
 
 // 获取店铺的总营业额
-export const getTotalPrice = () => axios.post('/CanteenWeb/Statistic/TotalPrice')
+export const getTotalPrice = () => axios.post('/CanteenWeb/StatisticAction/TotalPrice')
 
 // 统计本店铺的用户数量
-export const getUserNum = () => axios.post('/CanteenWeb/Statistic/TotalStudent')
+export const getUserNum = () => axios.post('/CanteenWeb/StatisticAction/TotalStudent')
 
 // 统计本店的累计订单数
-export const getOrderNum = () => axios.post('/CanteenWeb/Statistic/OrderCount')
+export const getOrderNum = () => axios.post('/CanteenWeb/StatisticAction/OrderCount')
 
 // 统计本店的菜品销售情况
-export const getStoreDishesNum = () => axios.post('/CanteenWeb/Statistic/StoreDishes')
+export const getStoreDishesNum = () => axios.post('/CanteenWeb/StatisticAction/StoreDishes')
 
 // 统计本店的违规订单数量
-export const getIllegalOrderNum = () => axios.post('/CanteenWeb/Statistic/IllegalOrder')
+export const getIllegalOrderNum = () => axios.post('/CanteenWeb/StatisticAction/IllegalOrder')
 
 // 获取本店的菜品数量
-export const getTotalDishesNum = () => axios.post('/CanteenWeb/Statistic/TotalDishes')
+export const getTotalDishesNum = () => axios.post('/CanteenWeb/StatisticAction/TotalDishes')
--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
   devServer: {
     proxy: { //配置跨域
       '/api': {
-        target: 'https://121.43.56.241', //这里后台的地址模拟的;应该填写你们真实的后台接口
+        target: 'https://www.51jiama.com', //这里后台的地址模拟的;应该填写你们真实的后台接口
         changOrigin: true, //允许跨域
         ws: true,
         pathRewrite: {
